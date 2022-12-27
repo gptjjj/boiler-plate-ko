d2a6fe46b1c9e9600a0d1285106d8635d19f513c
gptjjj <lifegoeson1010@naver.com>
2022-12-27 12:37:42 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gptjan:12345@boilerplate.7kd7wu0.mongodb.net/?retryWrites=true&w=majority', {
-  //ç  useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindandModify: false
+mongoose.set('strictQuery', true);
+mongoose.connect(config.mongoURI, {
+  //useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindandModify: false
 }).then(() => console.log('mongoDB connected..'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('안녕!세계!')
+  res.send('안녕!세계!!!!')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    //mongoDB에서 오는 메소드, user에 저장
+    user.save((err,userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
